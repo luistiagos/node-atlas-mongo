0367a4d9288055aa4ef2cbeb5d7a7afd1a1d0bee
luis.hablich <luis.hablich@serpro.gov.br>
2021-07-27 01:25:28 -0300

    props

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+const propertiesReader = require('properties-reader');
 
-const URI = "mongodb+srv://dbUser:dbUser@cluster0.hryvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var properties = propertiesReader('../props.txt');
+var connectionUrl = properties.get('connection.url');
+
+const URI = connectionUrl;//"mongodb+srv://dbUser:dbUser@cluster0.hryvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = () => {
    mongoose.connect(URI, {useUnifiedTopology: true, useNewUrlParser: true}).then(res => console.log("Connected to DB"))
